df35f4a71043d53c02c68682bb243c01c296befb
GaganTomarr <98637342+GaganTomarr@users.noreply.github.com>
2023-08-28 09:19:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser= require('body-parser');
+const path=require('path');
+
+const app = express();
+const PORT = 3007;
+app.set('Views',path.join(__dirname,'Views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname,'Public')));
+
+// Connect to the MongoDB database
+mongoose.connect('mongodb://localhost/logindata', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(express.urlencoded({ extended: true }));
+
+// Define a schema for the user collection
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+
+// Create a User model based on the schema
+const User = mongoose.model('User', userSchema);
+
+// Set up routes
+app.get('/', (req, res) => {
+  res.render('login');
+});
+
+app.post('/register', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const newUser = new User({
+      email,
+      password,
+    });
+
+    const savedUser = await newUser.save();
+    console.log('User saved successfully:', savedUser);
+    
+    res.render('page1', {email, password});
+    
+  } catch (error) {
+    console.error('Error saving user:',error);
+  }
+});
+
+app.get('/order', (req, res) => {
+  res.redirect('page2');
+});
+
+app.get('/page2', (req, res) => {
+  res.render('page2');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
